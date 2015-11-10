06497c128f11c019f4b39fc7b3bede719f8370c5
Alexander Babai <sustem34@gmail.com>
2015-11-10 23:15:47 +0300

    Add example of mixin
    Thanks to this portals for the useful information and source code.
    All rights belong to them.
    http://insightfullogic.com/2011/Sep/16/multiple-inheritance/
    http://justonjava.blogspot.com.by/2011/10/mixins-aka-traits-implementation-in.html
    
    And useful lib from here:
    http://mvnrepository.com/artifact/org.javassist/javassist/3.20.0-GA

---
+++ src/main/java/app/Mixin.java
@@ -0,0 +1,15 @@
+package app;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Mixin {
+	/**
+	 * Class implementing mixin interface.
+	 */
+	Class<?> impl();
+}
